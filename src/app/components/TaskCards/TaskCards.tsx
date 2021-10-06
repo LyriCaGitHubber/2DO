7c966071022298f7eb5e88ea48985e528d7737a4
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-10-06 22:04:54 +0200

    ✨ Create TaskCards component

--- src/app/components/TaskCards/TaskCards.tsx
+++ src/app/components/TaskCards/TaskCards.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import styles from './TaskCards.module.css';
 
 export type TaskCardProps = {
   task: string;
   id: number;
+  onClick: () => void;
 };
 
-export function TaskCards({ task, id }: TaskCardProps): JSX.Element {
+export function TaskCards({ task, id, onClick }: TaskCardProps): JSX.Element {
   return (
-    <section>
-      <p>{task}</p>
+    <section className={styles.taskCard}>
       <p>{id}</p>
+      <p>{task}</p>
+      <button onClick={onClick} className={styles.deleteButton}>
+        x
+      </button>
     </section>
   );
 }
