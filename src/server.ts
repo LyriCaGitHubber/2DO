e48c5aa95751c09a96c70fd83eb7350c07673e4e
LyriCaGitHubber <pf.munich.developer@gmail.com>
2021-11-01 16:52:23 +0100

    Initialize mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-import { MongoClient } from 'mongodb';
-
-const client = new MongoClient(process.env.MONGO_URL);
-
-async function run() {
-  try {
-    await client.connect();
-    await client.db('TasksDB').command({ ping: 1 });
-    console.log('DB Verbindung erfolgreich');
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 const app = express();
-const port = process.env.PORT || 3001;
 
 app.use(express.json());
+const port = process.env.PORT || 8000;
 
 app.get('/api/hello', (_request, response) => {
   response.send('Hello API!');
